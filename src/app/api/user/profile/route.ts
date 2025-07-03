f52a031439f6dc735a688374bac8d132b74757fa
alinomanriaz <alinomanriazch@gmail.com>
2025-07-03 17:58:09 +0500

    udpate

--- src/app/api/user/profile/route.ts
+++ src/app/api/user/profile/route.ts
@@ -1,23 +1,23 @@
-// // app/api/auth/me/route.ts
-// import { NextRequest, NextResponse } from 'next/server'
-// import jwt, { JwtPayload } from 'jsonwebtoken'
+// app/api/auth/me/route.ts
+import { NextRequest, NextResponse } from 'next/server'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
-// export async function GET(req: NextRequest) {
-//   const token = req.cookies.get('token')?.value
+export async function GET(req: NextRequest) {
+  const token = req.cookies.get('token')?.value
 
-//   if (!token) {
-//     return NextResponse.json({ message: 'Not authenticated' }, { status: 401 })
-//   }
+  if (!token) {
+    return NextResponse.json({ message: 'Not authenticated' }, { status: 401 })
+  }
 
-//   try {
-//     const decoded = jwt.verify(token, process.env.TOKEN_SECRET!) as JwtPayload // '!' tells TypeScript it's defined
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET!) as JwtPayload // '!' tells TypeScript it's defined
 
-//     return NextResponse.json({
-//       message: 'Authenticated',
-//       user: decoded,
-//     })
-//   } catch (error) {
-//     console.error('Error verifying token:', error)
-//     return NextResponse.json({ message: 'Invalid or expired token' }, { status: 401 })
-//   }
-// }
+    return NextResponse.json({
+      message: 'Authenticated',
+      user: decoded,
+    })
+  } catch (error) {
+    console.error('Error verifying token:', error)
+    return NextResponse.json({ message: 'Invalid or expired token' }, { status: 401 })
+  }
+}
