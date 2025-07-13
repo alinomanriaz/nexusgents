f77a6dae79f0d3d8398a512fdadb94155fb40555
alinomanriaz <alinomanriazch@gmail.com>
2025-07-13 19:18:04 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
-    domains:['avatar.vercel.sh']
+    domains:['avatar.vercel.sh','via.placeholder.com','source.unsplash.com','dummyimage.com','picsum.photos']
   }
 };
 
--- src/components/seo-page/BusinessGoal.tsx
+++ src/components/seo-page/BusinessGoal.tsx
@@ -1,31 +1,32 @@
 'use client'
 import { RiArrowRightSLine } from "react-icons/ri";
+import React, { useState } from 'react'
 
-import React, { useEffect, useState } from 'react'
+const content = [
+    {
+        heading: 'Packaging Industry US',
+        text: 'This is what happens when custom packaging meets smart funnels. Real orders. Real clients. Real results — straight from the US market.',
+        image: 'https://picsum.photos/600/400?random=3',
+    },
+    {
+        heading: 'Packaging industry Canada',
+        text: 'This is what happens when custom packaging meets smart funnels. Real orders. Real clients. Real results — straight from the US market.',
+        image: 'https://dummyimage.com/600x400/333/fff&text=Conversion+Boost',
+    },
+    {
+        heading: 'heading',
+        text: 'This is what happens when custom packaging meets smart funnels. Real orders. Real clients. Real results — straight from the US market.',
+        image: 'https://picsum.photos/600/400?random=3'
+    },
+    {
+        heading: 'heading',
+        text: 'This is what happens when custom packaging meets smart funnels. Real orders. Real clients. Real results — straight from the US market.',
+        image: 'https://dummyimage.com/600x400/333/fff&text=Conversion+Boost'
+    },
+]
 
 const BusinessGoal = () => {
     const [select, setSelect] = useState<number>(0)
-    const [displayImage, setDisplayImage] = useState<string>('bg-yellow-500')
-
-
-    useEffect(() => {
-        switch (select) {
-            case 0:
-                setDisplayImage('bg-yellow-500');
-                break;
-            case 1:
-                setDisplayImage('bg-red-500');
-                break;
-            case 2:
-                setDisplayImage('bg-green-500');
-                break;
-            case 3:
-                setDisplayImage('bg-blue-500');
-                break;
-            default:
-                setDisplayImage('bg-yellow-500');
-        }
-    }, [select]);
     return (
         <section className='flex flex-col justify-center items-center w-full '>
             <div className='w-11/12 h-full flex flex-col justify-between items-center'>
@@ -38,11 +39,11 @@ const BusinessGoal = () => {
                 <div className='grid grid-cols-2 w-full h-fit gap-6 my-10 '>
                     <div className="space-y-2.5">
                         {
-                            [...Array(4)].map((_, idx) => (
-                                <div key={idx} onMouseEnter={()=>setSelect(idx)} className={`card-style rounded-4xl flex flex-col w-full justify-between items-center px-8 py-6`}>
-                                    <div className="flex justify-between items-center w-full"><div className="text-2xl font-semibold ">Focused Efforts</div><RiArrowRightSLine className="size-6" /></div>
+                            content.map((data, idx) => (
+                                <div key={idx} onMouseEnter={() => setSelect(idx)} className={`card-style  transition-all duration-300 rounded-4xl flex flex-col w-full justify-between items-center px-8 py-6`}>
+                                    <div className="flex justify-between items-center w-full"><div className="text-2xl font-semibold ">{data.heading}</div><RiArrowRightSLine className="size-6" /></div>
                                     <div className='text-md w-full text-gray-600 dark:text-gray-500 pt-1 leading-6 tracking-wide'>
-                                        Unlock real growth with NexusGen&apos;s data-driven SEO strategies designed for long-term results, better traffic, and higher conversions.
+                                        {data.text}
                                     </div>
                                 </div>
                             ))
@@ -50,7 +51,11 @@ const BusinessGoal = () => {
                     </div>
                     <div>
                         <div className="w-full h-full flex flex-col justify-between items-center">
-                            <div className={`w-full h-full bg-gray-800 rounded-3xl ${displayImage} transition-all duration-1000 `}> .</div>
+                            {/* Background Image */}
+                            <div
+                                className="w-full h-full bg-center bg-cover bg-no-repeat rounded-3xl transition-all ease-in-out duration-300"
+                                style={{ backgroundImage: `url(${content[select].image})` }}
+                            ></div>
                             <div className="flex justify-between items-center mt-10">
                                 <div className="px-14">
                                     <div className="font-bold text-5xl ">300+</div>
